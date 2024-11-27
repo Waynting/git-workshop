699480e9f00e9bb0e9991a9a988849a03efceaa0
andre.liang <namwoam@gmail.com>
2024-11-27 13:56:31 +0800

    release: v0.0.1
    commit 48c2a1570e90e8a1e18718a8faced07fd0b63ea8
    Author: andre.liang <namwoam@gmail.com>
    Date:   Wed Nov 27 13:55:28 2024 +0800
    
        feat: support 3 number multiply
    
        commit efa68891098003237116967d9e0627b9cd1b3fa8
        Author: andre.liang <namwoam@gmail.com>
        Date:   Wed Nov 27 13:46:54 2024 +0800
    
            chore: add testcase for multiply(a,b,c)
    
        commit 737699dc63af83c1e3b62a2c68ecdc7cfe5a97f9
        Author: andre.liang <namwoam@gmail.com>
        Date:   Wed Nov 27 13:46:22 2024 +0800
    
            add: add multiply(a,b,c)
    
    commit 7be45f5fe51676a1f97505318b6d46562684fcfe
    Author: andre.liang <namwoam@gmail.com>
    Date:   Wed Nov 27 13:53:53 2024 +0800
    
        feat: the multiply function should be positive
    
        commit 8526b5534eb6ac6ab2d75e7d553a5c4a6686e5cd
        Author: andre.liang <namwoam@gmail.com>
        Date:   Wed Nov 27 13:51:56 2024 +0800
    
            chore: add testcase on the new logic
    
        commit 643dcd74e68cda0a28b2efe763b7c4d4abe047ae
        Author: andre.liang <namwoam@gmail.com>
        Date:   Wed Nov 27 13:51:01 2024 +0800
    
            fix: fix logic on multiply

--- main.cpp
+++ main.cpp
@@ -2,12 +2,23 @@
 
 int multiply(int a, int b)
 {
+    if (a * b < 0)
+    {
+        return 0;
+    }
     return a * b;
+}
+
+int multiply(int a, int b, int c)
+{
+    return a * b * c;
 }
 
 int main()
 {
     std::cout << "Hello 超級貓貓男!" << std::endl;
     std::cout << "5 * 6 = " << multiply(5, 6) << std::endl;
+    std::cout << "5 * 6 * 7 = " << multiply(5, 6, 7) << std::endl;
+    std::cout << "5 * -6 = " << multiply(5, -6) << std::endl;
     return 0;
 }
