4a06c9eac1121b1960756e495cabe1cc5b7b4c24
andre.liang <namwoam@gmail.com>
2024-11-27 13:36:31 +0800

    add: add multiply function

--- main.cpp
+++ main.cpp
@@ -1,6 +1,13 @@
 #include <iostream>
 
-int main(){
+int multiply(int a, int b)
+{
+    return a * b;
+}
+
+int main()
+{
     std::cout << "Hello 超級貓貓男!" << std::endl;
+    std::cout << "5 * 6 = " << multiply(5, 6) << std::endl;
     return 0;
 }
